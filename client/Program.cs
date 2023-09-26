321026478199d5b5f6d040543468bf97d2c58c88
bkmzli1 <bkmzlitel@gmail.com>
2023-09-26 11:01:52 +0500

    123

---
+++ client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("localhost", 1302);
+        Console.WriteLine("Connected");
+        NetworkStream stream = client.GetStream();
+        int
+            number = 5; // number to send, calculate and return factorial
+        string textToSend = number.ToString();
+        byte[] data = Encoding.ASCII.GetBytes(textToSend);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Data sent: " + textToSend);
+        client.Close();
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 1302);
+        server.Start();
+        Console.WriteLine("Server started...");
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Connected!");
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string receivedText = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            if (!int.TryParse(receivedText, out var number))
+            {
+                Console.WriteLine("Invalid number received, closing connection...");
+                client.Close();
+                continue;
+            }
+
+            Array.Clear(buffer, 0,
+                buffer.Length); // очистка буфера для следующего обмена
+            Console.WriteLine("Number received: {0}", number);
+            client.Close();
+        }
+    }
+}
