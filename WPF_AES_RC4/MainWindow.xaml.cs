fb841d43199a0b0d429eabd598709892d3c70b9a
bkmzli1 <bkmzlitel@gmail.com>
2023-10-11 14:38:03 +0500

    add

---
+++ WPF_AES_RC4/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace WPF_AES_RC4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AesOption.IsChecked = true;
+        }
+
+        private void Rc4Option_Checked(object sender, RoutedEventArgs e)
+        {
+            ivLabel.IsEnabled = false;
+            ivText.IsEnabled = false;
+        }
+
+        private void AesOption_Checked(object sender, RoutedEventArgs e)
+        {
+            ivLabel.IsEnabled = true;
+            ivText.IsEnabled = true;
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(keyText.Text) || string.IsNullOrEmpty(filePath.Text))
+                {
+                    MessageBox.Show("Password or File path cannot be empty.");
+                    return;
+                }
+
+                var keyDerivationFunction = new Rfc2898DeriveBytes(keyText.Text, Encoding.ASCII.GetBytes(ivText.Text));
+                byte[] key = keyDerivationFunction.GetBytes(32); // для AES256
+                byte[] iv = keyDerivationFunction.GetBytes(16); // для AES
+
+                if (AesOption.IsChecked == true)
+                {
+                    byte[] encrypted = EncryptStringToBytes_Aes(encryptText.Text, key, iv);
+                    File.WriteAllBytes(filePath.Text, encrypted);
+                }
+                else if (Rc4Option.IsChecked == true)
+                {
+                    byte[] encrypted = RC4.Encrypt(key, Encoding.UTF8.GetBytes(encryptText.Text));
+                    File.WriteAllBytes(filePath.Text, encrypted);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "An Error has Occurred");
+                return;
+            }
+            MessageBox.Show("всё записано", "инфо");
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(keyText.Text) || string.IsNullOrEmpty(filePath.Text))
+                {
+                    MessageBox.Show("Password or File path cannot be empty.");
+                    return;
+                }
+
+                byte[] cipherText = File.ReadAllBytes(filePath.Text);
+
+                var keyDerivationFunction = new Rfc2898DeriveBytes(keyText.Text, Encoding.ASCII.GetBytes(ivText.Text));
+                byte[] key = keyDerivationFunction.GetBytes(32); // для AES256
+                byte[] iv = keyDerivationFunction.GetBytes(16); // для AES
+
+                if (AesOption.IsChecked == true)
+                {
+                    string decrypted = DecryptStringFromBytes_Aes(cipherText, key, iv);
+                    MessageBox.Show(decrypted);
+                }
+                else if (Rc4Option.IsChecked == true)
+                {
+                    string decrypted = Encoding.UTF8.GetString(RC4.Decrypt(key, cipherText));
+                    MessageBox.Show(decrypted);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "An Error has Occurred");
+            }
+        }
+
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+
+            return encrypted;
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
+
+
+        private void savefile(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "BIN files (*.bin)|*.bin"; // Ограничить файлы только до .bin
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                filePath.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void openfile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "BIN files (*.bin)|*.bin"; // Ограничить файлы только до .bin
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
---
+++ WPF_AES_RC4/RC4.cs
@@ -0,0 +1,47 @@
+﻿namespace WPF_AES_RC4
+{
+    public class RC4
+    {
+        public static byte[] Encrypt(byte[] pwd, byte[] data)
+        {
+            int a, i, j, k;
+            byte[] key, box, cipher;
+            key = new byte[256];
+            box = new byte[256];
+            cipher = new byte[data.Length];
+            for (i = 0; i < 256; i++)
+            {
+                key[i] = pwd[i % pwd.Length];
+                box[i] = (byte)i;
+            }
+
+            for (i = j = 0; i < 256; i++)
+            {
+                j = (j + box[i] + key[i]) % 256;
+                byte temp = box[i];
+                box[i] = box[j];
+                box[j] = temp;
+            }
+
+            for (a = i = j = 0; i < data.Length; i++)
+            {
+                a++;
+                a %= 256;
+                j += box[a];
+                j %= 256;
+                byte temp = box[a];
+                box[a] = box[j];
+                box[j] = temp;
+                k = box[((box[a] + box[j]) % 256)];
+                cipher[i] = (byte)(data[i] ^ k);
+            }
+
+            return cipher;
+        }
+
+        public static byte[] Decrypt(byte[] pwd, byte[] data)
+        {
+            return Encrypt(pwd, data);
+        }
+    }
+}
